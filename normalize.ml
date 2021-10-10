996205749477e64f9ccb5e25ec04c1aec0ec6b93
kamahori <keisuke1258@gmail.com>
2021-10-10 23:24:37 +0900

    Rename functions in normalize.ml

--- normalize.ml
+++ normalize.ml
@@ -51,57 +51,58 @@ let insert_let (exp, typ) func =
       let exp', typ' = func x in
       (Let ((x, typ), exp, exp'), typ')
 
-let rec g env exp =
+let rec kNorm_ env exp =
   match exp with
   | Syntax.Unit -> (Unit, Type.Unit)
   | Syntax.Bool b -> (Int (if b then 1 else 0), Type.Int)
   | Syntax.Int i -> (Int i, Type.Int)
   | Syntax.Float f -> (Float f, Type.Float)
-  | Syntax.Not e -> g env (Syntax.If (e, Syntax.Bool false, Syntax.Bool true))
-  | Syntax.Neg e -> insert_let (g env e) (fun x -> (Neg x, Type.Int))
+  | Syntax.Not e ->
+      kNorm_ env (Syntax.If (e, Syntax.Bool false, Syntax.Bool true))
+  | Syntax.Neg e -> insert_let (kNorm_ env e) (fun x -> (Neg x, Type.Int))
   | Syntax.Add (e1, e2) ->
-      insert_let (g env e1) (fun x ->
-          insert_let (g env e2) (fun y -> (Add (x, y), Type.Int)))
+      insert_let (kNorm_ env e1) (fun x ->
+          insert_let (kNorm_ env e2) (fun y -> (Add (x, y), Type.Int)))
   | Syntax.Sub (e1, e2) ->
-      insert_let (g env e1) (fun x ->
-          insert_let (g env e2) (fun y -> (Sub (x, y), Type.Int)))
-  | Syntax.FNeg e -> insert_let (g env e) (fun x -> (Neg x, Type.Float))
+      insert_let (kNorm_ env e1) (fun x ->
+          insert_let (kNorm_ env e2) (fun y -> (Sub (x, y), Type.Int)))
+  | Syntax.FNeg e -> insert_let (kNorm_ env e) (fun x -> (Neg x, Type.Float))
   | Syntax.FAdd (e1, e2) ->
-      insert_let (g env e1) (fun x ->
-          insert_let (g env e2) (fun y -> (FAdd (x, y), Type.Float)))
+      insert_let (kNorm_ env e1) (fun x ->
+          insert_let (kNorm_ env e2) (fun y -> (FAdd (x, y), Type.Float)))
   | Syntax.FSub (e1, e2) ->
-      insert_let (g env e1) (fun x ->
-          insert_let (g env e2) (fun y -> (FSub (x, y), Type.Float)))
+      insert_let (kNorm_ env e1) (fun x ->
+          insert_let (kNorm_ env e2) (fun y -> (FSub (x, y), Type.Float)))
   | Syntax.FMul (e1, e2) ->
-      insert_let (g env e1) (fun x ->
-          insert_let (g env e2) (fun y -> (FMul (x, y), Type.Float)))
+      insert_let (kNorm_ env e1) (fun x ->
+          insert_let (kNorm_ env e2) (fun y -> (FMul (x, y), Type.Float)))
   | Syntax.FDiv (e1, e2) ->
-      insert_let (g env e1) (fun x ->
-          insert_let (g env e2) (fun y -> (FDiv (x, y), Type.Float)))
+      insert_let (kNorm_ env e1) (fun x ->
+          insert_let (kNorm_ env e2) (fun y -> (FDiv (x, y), Type.Float)))
   | Syntax.Eq (e1, e2) ->
-      g env
+      kNorm_ env
         (Syntax.If (Syntax.Eq (e1, e2), Syntax.Bool true, Syntax.Bool false))
   | Syntax.LE (e1, e2) ->
-      g env
+      kNorm_ env
         (Syntax.If (Syntax.LE (e1, e2), Syntax.Bool true, Syntax.Bool false))
-  | Syntax.If (Syntax.Not e1, e2, e3) -> g env (Syntax.If (e1, e3, e2))
+  | Syntax.If (Syntax.Not e1, e2, e3) -> kNorm_ env (Syntax.If (e1, e3, e2))
   | Syntax.If (Syntax.Eq (e1, e2), e3, e4) ->
-      insert_let (g env e1) (fun x ->
-          insert_let (g env e2) (fun y ->
-              let e3', t3 = g env e3 in
-              let e4', t4 = g env e4 in
+      insert_let (kNorm_ env e1) (fun x ->
+          insert_let (kNorm_ env e2) (fun y ->
+              let e3', t3 = kNorm_ env e3 in
+              let e4', t4 = kNorm_ env e4 in
               (IfEq (x, y, e3', e4'), t3)))
   | Syntax.If (Syntax.LE (e1, e2), e3, e4) ->
-      insert_let (g env e1) (fun x ->
-          insert_let (g env e2) (fun y ->
-              let e3', t3 = g env e3 in
-              let e4', t4 = g env e4 in
+      insert_let (kNorm_ env e1) (fun x ->
+          insert_let (kNorm_ env e2) (fun y ->
+              let e3', t3 = kNorm_ env e3 in
+              let e4', t4 = kNorm_ env e4 in
               (IfLE (x, y, e3', e4'), t3)))
   | Syntax.If (e1, e2, e3) ->
-      g env (Syntax.If (Syntax.Eq (e1, Syntax.Bool true), e2, e3))
+      kNorm_ env (Syntax.If (Syntax.Eq (e1, Syntax.Bool true), e2, e3))
   | Syntax.Let ((x, t), e2, e3) ->
-      let e2', t2 = g env e2 in
-      let e3', t3 = g (env_add x t env) e3 in
+      let e2', t2 = kNorm_ env e2 in
+      let e3', t3 = kNorm_ (env_add x t env) e3 in
       (Let ((x, t), e2', e3'), t2)
   | Syntax.Var x -> (
       if env_exists x env then (Var x, env_find x env)
@@ -115,8 +116,8 @@ let rec g env exp =
   | Syntax.LetRec
       ({ Syntax.name = x, t; Syntax.args = yts; Syntax.body = e1 }, e2) ->
       let env' = env_add x t env in
-      let e2', t2 = g env' e2 in
-      let e1', t1 = g (add_list yts env') e1 in
+      let e2', t2 = kNorm_ env' e2 in
+      let e1', t1 = kNorm_ (add_list yts env') e1 in
       (LetRec ({ name = (x, t); args = yts; body = e1' }, e2'), t2)
   | Syntax.App (Syntax.Var f, e2s) when not (env_exists f env) -> (
       match env_find f !TypeCheck.extenv with
@@ -125,19 +126,19 @@ let rec g env exp =
             match es with
             | [] -> (ExtFunApp (f, xs), t)
             | e2 :: e2s ->
-                insert_let (g env e2) (fun x -> bind (xs @ [ x ]) e2s)
+                insert_let (kNorm_ env e2) (fun x -> bind (xs @ [ x ]) e2s)
           in
           bind [] e2s
       | _ -> assert false)
   | Syntax.App (e1, e2s) -> (
-      match g env e1 with
+      match kNorm_ env e1 with
       | (_, Type.Fun (_, t)) as g_e1 ->
           insert_let g_e1 (fun f ->
               let rec bind xs es =
                 match es with
                 | [] -> (ExtFunApp (f, xs), t)
                 | e2 :: e2s ->
-                    insert_let (g env e2) (fun x -> bind (xs @ [ x ]) e2s)
+                    insert_let (kNorm_ env e2) (fun x -> bind (xs @ [ x ]) e2s)
               in
               bind [] e2s)
       | _ -> assert false)
@@ -146,17 +147,17 @@ let rec g env exp =
         match es with
         | [] -> (Tuple xs, Type.Tuple ts)
         | e :: es ->
-            let ex, tx = g env e in
+            let ex, tx = kNorm_ env e in
             insert_let (ex, tx) (fun x -> bind (xs @ [ x ]) (ts @ [ tx ]) es)
       in
       bind [] [] es
   | Syntax.LetTuple (xts, e1, e2) ->
-      insert_let (g env e1) (fun y ->
-          let e2', t2 = g (add_list xts env) e2 in
+      insert_let (kNorm_ env e1) (fun y ->
+          let e2', t2 = kNorm_ (add_list xts env) e2 in
           (LetTuple (xts, y, e2'), t2))
   | Syntax.Array (e1, e2) ->
-      insert_let (g env e1) (fun x ->
-          let ((_, t2) as g_e2) = g env e2 in
+      insert_let (kNorm_ env e1) (fun x ->
+          let ((_, t2) as g_e2) = kNorm_ env e2 in
           insert_let g_e2 (fun y ->
               let l =
                 match t2 with
@@ -165,17 +166,17 @@ let rec g env exp =
               in
               (ExtFunApp (l, [ x; y ]), Type.Array t2)))
   | Syntax.Get (e1, e2) -> (
-      match g env e1 with
+      match kNorm_ env e1 with
       | (_, Type.Array t) as g_e1 ->
           insert_let g_e1 (fun x ->
-              insert_let (g env e2) (fun y -> (Get (x, y), t)))
+              insert_let (kNorm_ env e2) (fun y -> (Get (x, y), t)))
       | _ -> assert false)
   | Syntax.Put (e1, e2, e3) ->
-      insert_let (g env e1) (fun x ->
-          insert_let (g env e2) (fun y ->
-              insert_let (g env e3) (fun z -> (Put (x, y, z), Type.Unit))))
+      insert_let (kNorm_ env e1) (fun x ->
+          insert_let (kNorm_ env e2) (fun y ->
+              insert_let (kNorm_ env e3) (fun z -> (Put (x, y, z), Type.Unit))))
 
-let f exp = fst (g [] exp)
+let kNorm exp = fst (kNorm_ [] exp)
 
 let rec print_env env =
   match env with
