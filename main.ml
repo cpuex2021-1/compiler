f3f4bf86503769c5086fa5215309756df81e6782
kamahori <keisuke1258@gmail.com>
2021-11-09 14:00:22 +0900

    Add type annotation comment

--- main.ml
+++ main.ml
@@ -6,30 +6,44 @@ let compile in_channel out_channel =
   if !verbose then (
     print_endline "[Parsed]";
     print_endline (Syntax.print parsed));
+
+  (* Syntax.t -> Syntax.t *)
   let typed = TypeCheck.f parsed in
   if !verbose then (
     print_endline "[Typed]";
     print_endline (Syntax.print typed));
+
+  (* Syntax.t -> Normalize.t *)
   let normalized = Normalize.kNorm typed in
   if !verbose then (
     print_endline "[Normalized]";
     print_endline (Normalize.print normalized));
+
+  (* Normalize.t -> Normalize.t *)
   let transformed = Normalize.alpha normalized in
   if !verbose then (
     print_endline "[Alpha Transformed]";
     print_endline (Normalize.print transformed));
+
+  (* Normalize.t -> Closure.prog *)
   let closured = Closure.f transformed in
   if !verbose then (
     print_endline "[Closured]";
     print_endline (Closure.print closured));
+
+  (* Closure.prog -> Asm.prog *)
   let virtual_asm = Virtual.f closured in
   if !verbose then (
     print_endline "[Virtual Asm]";
     print_endline (Virtual.print virtual_asm));
+
+  (* Asm.prog -> Asm.prog *)
   let allocated = RegAlloc.f virtual_asm in
   if !verbose then (
     print_endline "[Register Allocated]";
     print_endline (Virtual.print allocated));
+
+  (* Asm.prog -> output *)
   Emit.f out_channel allocated
 
 let file f =
