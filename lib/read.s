7f7603ed819b6c9b32374fafc00e3fb4fb2ae78a
kamahori <keisuke1258@gmail.com>
2022-01-04 15:16:34 +0900

    [wip] Fix read.s

--- lib/read.s
+++ lib/read.s
@@ -1,6 +1,25 @@
 read_int:
     lw a0, 0(zero)
-	jalr zero, ra, 0
+    lw a1, 0(zero)
+    sli a1, a1, 8
+    or a0, a0, a1
+    lw a1, 0(zero)
+    sli a1, a1, 16
+    or a0, a0, a1
+    lw a1 , 0(zero)
+    sli a1, a1, 24
+    or a0, a0, a1
+    jalr zero, ra, 0
 read_float:
-    lw f0, 0(zero)
+    lw a0, 0(zero)
+    lw a1, 0(zero)
+    sli a1, a1, 8
+    or a0, a0, a1
+    lw a1, 0(zero)
+    sli a1, a1, 16
+    or a0, a0, a1
+    lw a1 , 0(zero)
+    sli a1, a1, 24
+    or a0, a0, a1
+    fmv.w.x f0, a0
 	jalr zero, ra, 0
