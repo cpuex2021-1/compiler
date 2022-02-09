3fdd93e17b01aa857f9da1f9f7fc0d3e58176382
Keisuke Kamahori <keisuke1258@gmail.com>
2022-02-09 18:00:34 +0900

    Not WAR, but RAW for VLIW

--- emit.ml
+++ emit.ml
@@ -512,9 +512,9 @@ let is_depend a b =
       intersect rd_a rs_b (* RAW *)
       || intersect rd_b rs_a (* WAR *)
       || intersect rd_a rd_b
-
 (* WAW *)
-let is_depend_war a b =
+
+let is_depend_raw a b =
   match (a, b) with
   (* we suppose here that no two adjacent
      store instructions are about the same address *)
@@ -525,10 +525,9 @@ let is_depend_war a b =
          true *)
   | Nop, _ | _, Nop -> false
   | _, _ ->
-      let rd_b = rd b in
-      let rs_a = rs a in
-      intersect rd_b rs_a
-(* WAR *)
+      let rd_a = rd a in
+      let rs_b = rs b in
+      intersect rd_a rs_b
 
 let rec n_in graph i =
   match graph with
@@ -786,10 +785,10 @@ let rec print oc insns tmp =
           print oc rest tmp)
       else
         let depend =
-          is_depend_war tmp.(0) cur
-          || is_depend_war tmp.(1) cur
-          || is_depend_war tmp.(2) cur
-          || is_depend_war tmp.(3) cur
+          is_depend_raw tmp.(0) cur
+          || is_depend_raw tmp.(1) cur
+          || is_depend_raw tmp.(2) cur
+          || is_depend_raw tmp.(3) cur
           || (ty = 1 && tmp.(0) != Nop && tmp.(1) != Nop)
           || (ty = 2 && tmp.(0) != Nop && tmp.(1) != Nop)
           || (ty = 3 && tmp.(2) != Nop && tmp.(3) != Nop)
